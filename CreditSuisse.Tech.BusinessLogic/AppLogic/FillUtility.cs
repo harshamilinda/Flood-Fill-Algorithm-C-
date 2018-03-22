99fab4a37b3159f340b23a9ce00b933b04119847
harshamilinda <harshamilinda@gmail.com>
2018-03-22 12:43:47 +0800

    updates

--- CreditSuisse.Tech.BusinessLogic/AppLogic/FillUtility.cs
+++ CreditSuisse.Tech.BusinessLogic/AppLogic/FillUtility.cs
@@ -11,29 +11,23 @@ namespace CreditSuisse.Tech.BusinessLogic
     public class FillUtility : IFillable
     {
         public List<DataLine> Canvas { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
         public char FillColour { get; set; }
 
         public T Fill<T>(T canvas, Dictionary<ConsoleCommand, string> instructions) where T : List<DataLine>, new()
         {
-            //B 10 3 o
             Canvas = canvas;
             FillColour = char.Parse(instructions[ConsoleCommand.Colour]);
             var Positions = instructions.GetPositions();
-            X = Positions[Axis.X1];
-            Y = Positions[Axis.Y1];
-            var DataPoint = new DataPoint { X = X, Y = Y };
-            Octa(DataPoint);
-
-
+            var DataPoint = new DataPoint { X = Positions[Axis.X1], Y = Positions[Axis.Y1] };
+            if (IsValidDataPoint(DataPoint))
+                Octa(DataPoint);
 
             return canvas;
         }
+        private bool IsValidDataPoint(DataPoint d) => (char.IsWhiteSpace(Canvas[d.Y].Line[d.X]));
 
         private void ProcessDataPoint(int x, int y, Stack<DataPoint> dataPoints)
         {
-            // Validate data range
             if (Canvas.Count > y && Canvas.FirstOrDefault().Line.Length > x)
             {
                 if (char.IsWhiteSpace(Canvas[y].Line[x]))
@@ -41,72 +35,39 @@ namespace CreditSuisse.Tech.BusinessLogic
                     Canvas[y].Line[x] = FillColour;
                     dataPoints.Push(new DataPoint { X = x, Y = y });
                 }
-
             }
-
-
-
-
         }
+        /// <summary>
+        /// Flood Fill
+        /// </summary>
+        /// <param name="d">DataPoint</param>
         private void Octa(DataPoint d)
         {
             var Items = new Stack<DataPoint>();
-            // is empty
             if (char.IsWhiteSpace(Canvas[d.Y].Line[d.X]))
-                //fill - only for the fist time
                 Canvas[d.Y].Line[d.X] = FillColour;
-            //do not add
+            
+            //Top
+            ProcessDataPoint(d.X, d.Y - 1, Items);
+            //Top Left
+            ProcessDataPoint(d.X - 1, d.Y - 1, Items);
+            //Top Right
+            ProcessDataPoint(d.X + 1, d.Y - 1, Items);
+            //Previous
+            ProcessDataPoint(d.X - 1, d.Y, Items);
+            //Next
+            ProcessDataPoint(d.X + 1, d.Y, Items);
+            //Down
+            ProcessDataPoint(d.X, d.Y + 1, Items);
+            //Down Left
+            ProcessDataPoint(d.X - 1, d.Y + 1, Items);
+            //Down Right
+            ProcessDataPoint(d.X + 1, d.Y + 1, Items);
 
-            //is top
-            //if (char.IsWhiteSpace(Canvas[d.Y - 1].Line[d.X]))
-                ProcessDataPoint(d.X, d.Y - 1, Items);
-
-            //is topleft
-            //if (char.IsWhiteSpace(Canvas[d.Y - 1].Line[d.X - 1]))
-                ProcessDataPoint(d.X-1, d.Y - 1, Items);
-            //Fill
-            //Add
-
-
-            //is topright
-            //if (char.IsWhiteSpace(Canvas[d.Y - 1].Line[d.X + 1]))
-                ProcessDataPoint(d.X+1, d.Y - 1, Items);
-
-
-            //previous
-            //if (char.IsWhiteSpace(Canvas[d.Y].Line[d.X - 1]))
-                ProcessDataPoint(d.X-1, d.Y, Items);
-
-
-            //next
-            //if (char.IsWhiteSpace(Canvas[d.Y].Line[d.X + 1]))
-                ProcessDataPoint(d.X + 1, d.Y, Items);
-
-            //down
-            //if (char.IsWhiteSpace(Canvas[d.Y + 1].Line[d.X]))
-                ProcessDataPoint(d.X, d.Y + 1, Items);
-
-
-            //downLeft
-            //if (char.IsWhiteSpace(Canvas[d.Y + 1].Line[d.X - 1]))
-                ProcessDataPoint(d.X - 1, d.Y + 1, Items);
-
-
-            //downright
-            //if (char.IsWhiteSpace(Canvas[d.Y + 1].Line[d.X + 1]))
-                ProcessDataPoint(d.X + 1, d.Y + 1, Items);
-
-
-
-
-            //add to datapoint array
-
-            //Not return 8 all the time
-            while(Items.Count >0)
+            while (Items.Count > 0)
             {
                 Octa(Items.Pop());
             }
-           
         }
 
     }
